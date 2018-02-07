3e4c6f65e96bc31bbe6a9a34ae8e0dc8126fdc99
Mady974 <t.traorebastida@rubika-edu.com>
2018-02-07 09:59:39 +0100

    y

--- Assets/Scripts/AiConvoi.cs
+++ Assets/Scripts/AiConvoi.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-<<<<<<< HEAD
-=======
 
->>>>>>> 8e83927a31948324631c196be8ede36d8fed917a
 
 public class AiConvoi : MonoBehaviour {
 
